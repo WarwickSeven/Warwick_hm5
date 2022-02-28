8fa5a6aa5798ec13a68db8d52be03c016aa9de27
WarwickSeven <WarwickSeven@gmail.com>
2022-02-28 17:16:22 +0300

    домашнее задание 5-1

--- Warwick_hm5/main.cpp
+++ Warwick_hm5/main.cpp
@@ -1,14 +1,27 @@
-//
-//  main.cpp
-//  Warwick_hm5
-//
-//  Created by Александр Тихонов on 28.02.2022.
-//
-
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using std::cout;
+using std::endl;
+
+// домашнее задание 5-1
+
+bool PrintArray(double arr[], int size)
+{
+    for (size_t i = 0; i < size; i++)
+    {
+        cout << arr[i] << endl;
+    }
+    cout << endl;
+    return true;
+}
+int main()
+{
+    const int mysize = 5;
+    double myarr[] = { 2.25, 3.35, 4.45, 5.55, 6.65 };
+    
+    if (PrintArray(myarr, mysize))
+    {
+        cout << "Array was sucesfully printed" << endl;
+    }
     return 0;
 }
